9f9376b4bfbee229d7b993a4256fddaae2e1d7b4
Mounir-Dev <abdennourmounir5@gmail.com>
2025-06-14 02:13:06 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <MainLayout>
+    <router-view />
+  </MainLayout>
+</template>
+
+<script>
+import MainLayout from './layout/MainLayout.vue'
+
+export default {
+  components: {
+    MainLayout
+  }
+}
+</script>
---
+++ src/layout/MainLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="wrapper">
+    <Header />
+    <main>
+      <slot />
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+import Footer from '../components/Footer.vue'
+
+export default {
+  name: 'MainLayout',
+  components: {
+    Header,
+    Footer
+  },
+  mounted() {
+    // Si des scripts JS externes doivent être appelés
+    setTimeout(() => {
+      if (window?.init) {
+        window.init(); // exemple : fonction JS définie dans functions.bundle.js
+      }
+    }, 100);
+  }
+}
+</script>
+
